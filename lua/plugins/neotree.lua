5465b9369b0837e6fdb615e63eefaa2f75da6940
Anurag Singh <contact@anurag.sh>
2024-04-20 18:58:31 +0800

    hide .git

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -21,6 +21,9 @@ return {
           filtered_items = {
             hide_dotfiles = false,
             hide_gitignored = false,
+            hide_by_name = {
+              '.git',
+            },
           },
         },
       }
