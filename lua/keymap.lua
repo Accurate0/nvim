de22dacd5e0d27f954df601cb1691fc1e5a0e0af
Anurag Singh <contact@anurag.sh>
2024-04-20 18:50:41 +0800

    fix lmao

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -24,14 +24,17 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
 vim.keymap.set('n', '<C-Left>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-Right>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-Down>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-Up>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 vim.keymap.set('n', '<leader>L', require('lazy').show, { desc = 'Open Lazy' })
+
+vim.keymap.set('n', '<leader>e', function()
+  require('neo-tree.command').execute { toggle = true }
+end, { desc = 'Toggle neotree' })
+
+vim.keymap.set('n', '<C-/>', function()
+  require('Comment.api').toggle.linewise.current()
+end, { noremap = true, silent = true })
---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons',
+      'MunifTanjim/nui.nvim',
+      '3rd/image.nvim',
+    },
+    config = function()
+      require('neo-tree').setup {
+        default_component_configs = {
+          icon = {
+            folder_closed = ' ',
+            folder_open = ' ',
+            folder_empty = '󰜌',
+          },
+        },
+        filesystem = {
+          filtered_items = {
+            hide_dotfiles = false,
+            hide_gitignored = false,
+          },
+        },
+      }
+    end,
+  },
+}
