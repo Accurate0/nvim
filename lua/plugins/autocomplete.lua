17bcfda31a87c9664ca65eddfb0c60273cd1b8b5
Anurag Singh <contact@anurag.sh>
2024-04-28 14:36:25 +0800

    enable snippets

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -21,15 +21,12 @@ return {
           return 'make install_jsregexp'
         end)(),
         dependencies = {
-          -- `friendly-snippets` contains a variety of premade snippets.
-          --    See the README about individual language/framework/plugin snippets:
-          --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
         },
       },
       'saadparwaiz1/cmp_luasnip',
@@ -48,7 +45,7 @@ return {
       luasnip.config.setup {}
 
       cmp.setup {
-        ---@diagnostic disable: missing-fields
+        ---@diagnostic disable-next-line: missing-fields
         formatting = {
           format = require('lspkind').cmp_format {
             mode = 'symbol',
@@ -69,7 +66,6 @@ return {
             end,
           },
         },
-        --@diagnostic enable: missing-fields
         snippet = {
           expand = function(args)
             luasnip.lsp_expand(args.body)
