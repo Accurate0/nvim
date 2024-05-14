ed920b5ce40350a4dfc69c807e809f9bf823bf16
Anurag Singh <contact@anurag.sh>
2024-05-14 14:37:57 +0800

    idont rememebr

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -2,11 +2,7 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
--- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
--- vim.keymap.set('n', '<leader>q', function()
---   vim.diagnostic.setloclist { open = true }
--- end, { desc = 'Open diagnostic [Q]uickfix list' })
-
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -28,6 +28,7 @@ return {
         'oil',
         'undotree',
         'Trouble',
+        'trouble',
       },
     },
   },
