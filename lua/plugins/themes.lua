52864d1c66ed18a4eb918ef4f10b16c4f001b8aa
Anurag Singh <contact@anurag.sh>
2024-05-14 11:09:44 +0800

    add themes i wont use

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -10,4 +10,14 @@ return {
     priority = 1000,
   },
   { 'loctvl842/monokai-pro.nvim', priority = 1000 },
+  { 'rose-pine/neovim', name = 'rose-pine' },
+  {
+    'ribru17/bamboo.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('bamboo').setup {}
+      require('bamboo').load()
+    end,
+  },
 }
