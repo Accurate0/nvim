08db9505b05911305e98a9053536b687392f09e8
Anurag Singh <contact@anurag.sh>
2024-05-17 15:48:48 +0800

    remove unusued themes

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -5,19 +5,4 @@ return {
     priority = 1000,
     lazy = false,
   },
-  {
-    'folke/tokyonight.nvim',
-    priority = 1000,
-  },
-  { 'loctvl842/monokai-pro.nvim', priority = 1000 },
-  { 'rose-pine/neovim', name = 'rose-pine' },
-  {
-    'ribru17/bamboo.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require('bamboo').setup {}
-      require('bamboo').load()
-    end,
-  },
 }
