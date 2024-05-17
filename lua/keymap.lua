3789ae4b5324d524d4b6a3b72b017d0317de1fd6
Anurag Singh <contact@anurag.sh>
2024-05-17 18:06:51 +0800

    comment some keymaps

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,7 +1,7 @@
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+-- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+-- vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
@@ -17,9 +17,9 @@ vim.keymap.set('n', '<leader>w', function()
   require('neo-tree.command').execute { toggle = true }
 end, { desc = 'Toggle neotree' })
 
-vim.keymap.set('n', '<A-1>', '<Cmd>BufferPrevious<CR>', { desc = 'Previous Tab' })
-vim.keymap.set('n', '<A-2>', '<Cmd>BufferNext<CR>', { desc = 'Next Tab' })
-vim.keymap.set('n', '<A-w>', '<Cmd>BufferClose<CR>', { desc = 'Close Tab' })
+-- vim.keymap.set('n', '<A-1>', '<Cmd>BufferPrevious<CR>', { desc = 'Previous Tab' })
+-- vim.keymap.set('n', '<A-2>', '<Cmd>BufferNext<CR>', { desc = 'Next Tab' })
+-- vim.keymap.set('n', '<A-w>', '<Cmd>BufferClose<CR>', { desc = 'Close Tab' })
 
 vim.keymap.set('n', '<leader>tp', require('precognition').toggle, { desc = '[T]oggle [P]recognition' })
 
