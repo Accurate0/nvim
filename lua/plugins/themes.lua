46f3d5c1f9cb50e2de1bb30dc1c9c14ee7dd444c
Anurag Singh <contact@anurag.sh>
2025-01-19 19:08:27 +0800

    change theme perhaps

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,6 +1,12 @@
 ---@type LazySpec
 return {
   {
+    'mellow-theme/mellow.nvim',
+    name = 'mellow',
+    priority = 1000,
+    lazy = false,
+  },
+  {
     'catppuccin/nvim',
     name = 'catppuccin',
     priority = 1000,
