481fa195a6179b2c90ffd7930f4cc52bfd2499da
Anurag Singh <contact@anurag.sh>
2026-05-13 19:04:59 +0800

    fix treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,10 +2,10 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
-    dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
+    branch = 'main',
     build = ':TSUpdate',
-    opts = {
-      ensure_installed = {
+    init = function()
+      local ensureInstalled = {
         'bash',
         'c',
         'html',
@@ -15,14 +15,37 @@ return {
         'vim',
         'vimdoc',
         'rust',
-      },
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-      textobjects = {
+      }
+
+      local alreadyInstalled = require('nvim-treesitter.config').get_installed()
+      local parsersToInstall = vim.iter(ensureInstalled)
+        :filter(function(parser)
+          return not vim.tbl_contains(alreadyInstalled, parser)
+        end)
+        :totable()
+
+      if #parsersToInstall > 0 then
+        require('nvim-treesitter').install(parsersToInstall)
+      end
+
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function()
+          pcall(vim.treesitter.start)
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+
+      vim.filetype.add {
+        pattern = { ['.*/hypr/.*%.conf'] = 'hyprlang' },
+      }
+    end,
+  },
+  {
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    branch = 'main',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    config = function()
+      require('nvim-treesitter-textobjects').setup {
         move = {
           enable = true,
           goto_next_start = { [']f'] = '@function.outer' },
@@ -30,15 +53,6 @@ return {
           goto_previous_start = { ['[f'] = '@function.outer' },
           goto_previous_end = { ['[F'] = '@function.outer' },
         },
-      },
-    },
-    config = function(_, opts)
-      require('nvim-treesitter.install').prefer_git = true
-      ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup(opts)
-
-      vim.filetype.add {
-        pattern = { ['.*/hypr/.*%.conf'] = 'hyprlang' },
       }
     end,
   },
